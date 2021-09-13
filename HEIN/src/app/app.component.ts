e6f261c71ac958ae9d671034dd2e712cdc04f5d3
Ruby Faucher <ruby.faucher@gmail.com>
2021-09-13 15:49:09 +0200

    get patient in front

--- HEIN/src/app/app.component.ts
+++ HEIN/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HEIN';
+  private apiURL = "https://fhir.eole-consulting.io/api/patient/613f4788a5b46400122cf50e";
+  data: any = {};
+  constructor(private http: HttpClient) {
+    console.log("hello");
+    this.getData();
+  }
+  getData() {
+    return (this.http.get(this.apiURL).forEach(patient => { console.log(patient); this.data = patient; }));
+
+  }
+  getPatient() {
+
+  }
 }
