f2317c6f9a44f9d6fb1466a0cacb2bb82c8b361e
Nicolas <nicolas.jacomi@etud.univ-jfc.fr>
2021-09-13 17:58:58 +0200

    up

--- HEIN/src/app/app.module.ts
+++ HEIN/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
@@ -17,7 +20,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
 
   providers: [AppComponent],
