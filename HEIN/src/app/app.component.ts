b3c7dec1594b55fdf90c6ce8d1a7f9b4bb201a9e
Ruby Faucher <ruby.faucher@gmail.com>
2021-09-14 11:26:37 +0200

    add user profile modal

--- HEIN/src/app/app.component.ts
+++ HEIN/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from './modal/modal.component';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,22 +16,28 @@ export class AppComponent {
   private apiURL = "https://fhir.eole-consulting.io/api";
   patient: any = {};
   doctor: any = {};
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,public matDialog: MatDialog) {
     console.log("hello");
     this.getPatient();
     this.getPractitionner();
-    this.getCommunication();
+
+  }
+  openModal() {
+    const dialogConfig = new MatDialogConfig();
+    //dialogConfig.disableClose = true;
+    dialogConfig.id = "modal-component";
+    dialogConfig.height = "350px";
+    dialogConfig.width = "600px";
+    dialogConfig.data = this.patient;
+    const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
   }
   getPatient() {
-    return (this.http.get(this.apiURL+'/patient/613f4788a5b46400122cf50e').forEach(patient => { console.log(patient); this.patient = patient; }));
+    return (this.http.get(this.apiURL+'/patient/613f4788a5b46400122cf50e').forEach(patient => {  this.patient = patient; }));
 
   }
   getPractitionner() {
-    return (this.http.get(this.apiURL+'/practitioner/613f51d8a5b46400122cf511').forEach(doctor => { console.log(doctor); this.doctor = doctor; }));
+    return (this.http.get(this.apiURL+'/practitioner/613f51d8a5b46400122cf511').forEach(doctor => {  this.doctor = doctor; }));
 
   }
-  getCommunication() {
-    return (this.http.get(this.apiURL+'/communication').forEach(comm => { console.log(comm);  }));
 
-  }
 }
---
+++ HEIN/src/app/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  patient: any = {};
+
+
+  constructor(@Inject(MAT_DIALOG_DATA) data) {
+    this.patient = data;
+  }
+
+  ngOnInit(): void {
+    console.log(this.patient);
+  }
+
+}
